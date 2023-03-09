a08fcad5c8238ca751cb7f7c95f194e5855107b9
mhmmd <mhmmd.rezaei4@gmail.com>
2023-03-09 17:40:44 +0330

    Calss ArgumentError and OverheatingError

--- factory-sensors.js
+++ factory-sensors.js
@@ -1,1 +1,10 @@
 // i cant understand (Error and Inheritance) look back again and again 😑😔😌😪
+
+class ArgumentError extends Error {}
+
+class OverheatingError extends Error {
+  constructor(temperature) {
+    super(`The temperature is ${temperature} ! Overheating !`);
+    this.temperature = temperature;
+  }
+}
